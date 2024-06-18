e173e9b7c26f332c7e6dbf3dd6e2e3f2dc538524
murLarva <158815368+murLarva@users.noreply.github.com>
2024-06-18 10:20:05 +0300

    Add files via upload

---
+++ зад1.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://wttr.in'  
+weather_parameters = {
+    '0': '',
+    'T': ''
+}
+
+response = requests.get(url, params=weather_parameters)
+
+print(response.text)
